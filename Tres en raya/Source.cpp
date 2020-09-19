ff8f368c08ffb6e582a96b23fb2aa5e400f832f7
Daniel Camacho <daniel.camacho.santacruz.5c@gmail.com>
2020-09-18 21:28:55 -0400

    refactorizado

--- Tres en raya/Source.cpp
+++ Tres en raya/Source.cpp
@@ -2,143 +2,126 @@
 using namespace std;
 
 
-char tab[10] = { 'o','1','2','3','4','5','6','7','8','9' };
 
+char Board[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+// El metodo mostrar Board en una funcion aparte
+void mostrarTablero(char Board[10])
+{
+    system("cls");
+    cout << "\n\n\Tres en raya\n\n";
+    cout << "Jugador 1 (X)  -  Jugador 2 (O)" << endl << endl;
+    cout << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << Board[1] << "  |  " << Board[2] << "  |  " << Board[3] << endl;
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << Board[4] << "  |  " << Board[5] << "  |  " << Board[6] << endl;
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << Board[7] << "  |  " << Board[8] << "  |  " << Board[9] << endl;
+    cout << "     |     |     " << endl << endl;
+}
+// Ver si hay ganador
+bool checkWinner()
+{
+    bool existsWinner=false;
+    if (Board[1] == Board[2] && Board[2] == Board[3])
+        existsWinner = true;
 
+    else if (Board[4] == Board[5] && Board[5] == Board[6])
+
+        existsWinner = true;
+    else if (Board[7] == Board[8] && Board[8] == Board[9])
+
+        existsWinner = true;
+    else if (Board[1] == Board[4] && Board[4] == Board[7])
+
+        existsWinner = true;
+    else if (Board[2] == Board[5] && Board[5] == Board[8])
+
+        existsWinner = true;
+    else if (Board[3] == Board[6] && Board[6] == Board[9])
+
+        existsWinner = true;
+    else if (Board[1] == Board[5] && Board[5] == Board[9])
+
+        existsWinner = true;
+    else if (Board[3] == Board[5] && Board[5] == Board[7])
+
+        existsWinner = true;
+    return existsWinner;
+}
+
+// Metodo para realizar una jugada
+bool checkMovement(int Position, char identifier)
+{
+
+    bool IsSuccesed = true;
+    if (Position == 1 && Board[1] == '1')
+        Board[1] = identifier;
+    else if (Position == 2 && Board[2] == '2')
+        Board[2] = identifier;
+    else if (Position == 3 && Board[3] == '3')
+        Board[3] = identifier;
+    else if (Position == 4 && Board[4] == '4')
+        Board[4] = identifier;
+    else if (Position == 5 && Board[5] == '5')
+        Board[5] = identifier;
+    else if (Position == 6 && Board[6] == '6')
+        Board[6] = identifier;
+    else if (Position == 7 && Board[7] == '7')
+        Board[7] = identifier;
+    else if (Position == 8 && Board[8] == '8')
+        Board[8] = identifier;
+    else if (Position == 9 && Board[9] == '9')
+        Board[9] = identifier;
+    else
+        IsSuccesed = false;
+
+    return IsSuccesed;
+}
+
+void makeMovement(int Position, char identifier, int *Player)
+{
+    if (!checkMovement(Position, identifier))
+    {
+        cout << "Jugada no permitida";
+        *Player--;
+        cin.ignore();
+        cin.get();
+    }
+}
+void showresult(bool existsWinner, int Player)
+{
+    if (existsWinner)
+        cout << "==>\aJugador " << Player-1 << " gana ";
+    else
+        cout << "==>\aEmpate!";
+    cin.ignore();
+    cin.get();
+}
 
 int main()
 {
-    int jug = 1;
-    int i;
-    int des;
-    char mar;
+    int Player = 1; //nombres Descriptivos   
+    bool existsWinner = false; //Cambio de tipo para ver ganador
+    int Position;  //nombres Descriptivos  
+    int jugadas=0;  //nombres Descriptivos  
+    char identifier;  //nombres Descriptivos  
     do
     {
-        system("cls");
-        cout << "\n\n\Tres en raya\n\n";
-
-        cout << "Jugador 1 (X)  -  Jugador 2 (O)" << endl << endl;
-        cout << endl;
-
-        cout << "     |     |     " << endl;
-        cout << "  " << tab[1] << "  |  " << tab[2] << "  |  " << tab[3] << endl;
-
-        cout << "_____|_____|_____" << endl;
-        cout << "     |     |     " << endl;
-
-        cout << "  " << tab[4] << "  |  " << tab[5] << "  |  " << tab[6] << endl;
-
-        cout << "_____|_____|_____" << endl;
-        cout << "     |     |     " << endl;
-
-        cout << "  " << tab[7] << "  |  " << tab[8] << "  |  " << tab[9] << endl;
-
-        cout << "     |     |     " << endl << endl;
-
-        jug = (jug % 2) ? 1 : 2;
-
-        cout << "Jugador " << jug << ", ingresa un numero:  ";
-        cin >> des;
-
-        mar = (jug == 1) ? 'X' : 'O';
-
-        if (des == 1 && tab[1] == '1')
-
-            tab[1] = mar;
-        else if (des == 2 && tab[2] == '2')
-
-            tab[2] = mar;
-        else if (des == 3 && tab[3] == '3')
-
-            tab[3] = mar;
-        else if (des == 4 && tab[4] == '4')
-
-            tab[4] = mar;
-        else if (des == 5 && tab[5] == '5')
-
-            tab[5] = mar;
-        else if (des == 6 && tab[6] == '6')
-
-            tab[6] = mar;
-        else if (des == 7 && tab[7] == '7')
-
-            tab[7] = mar;
-        else if (des == 8 && tab[8] == '8')
-
-            tab[8] = mar;
-        else if (des == 9 && tab[9] == '9')
-
-            tab[9] = mar;
-        else
-        {
-            cout << "Jugada no permitida";
-
-            jug--;
-            cin.ignore();
-            cin.get();
-        }
-        if (tab[1] == tab[2] && tab[2] == tab[3])
-
-            i= 1;
-        else if (tab[4] == tab[5] && tab[5] == tab[6])
-
-            i= 1;
-        else if (tab[7] == tab[8] && tab[8] == tab[9])
-
-            i=1 ;
-        else if (tab[1] == tab[4] && tab[4] == tab[7])
-
-            i= 1;
-        else if (tab[2] == tab[5] && tab[5] == tab[8])
-
-            i= 1;
-        else if (tab[3] == tab[6] && tab[6] == tab[9])
-
-            i= 1;
-        else if (tab[1] == tab[5] && tab[5] == tab[9])
-
-            i= 1;
-        else if (tab[3] == tab[5] && tab[5] == tab[7])
-
-            i= 1;
-        else if (tab[1] != '1' && tab[2] != '2' && tab[3] != '3'
-            && tab[4] != '4' && tab[5] != '5' && tab[6] != '6'
-            && tab[7] != '7' && tab[8] != '8' && tab[9] != '9')
-
-            i= 0;
-        else
-            i= -1;
-
-        jug++;
-    } while (i == -1);
-    system("cls");
-    cout << "\n\n\Tres en raya\n\n";
-
-    cout << "Jugador 1 (X)  -  Jugador 2 (O)" << endl << endl;
-    cout << endl;
-
-    cout << "     |     |     " << endl;
-    cout << "  " << tab[1] << "  |  " << tab[2] << "  |  " << tab[3] << endl;
-
-    cout << "_____|_____|_____" << endl;
-    cout << "     |     |     " << endl;
-
-    cout << "  " << tab[4] << "  |  " << tab[5] << "  |  " << tab[6] << endl;
-
-    cout << "_____|_____|_____" << endl;
-    cout << "     |     |     " << endl;
-
-    cout << "  " << tab[7] << "  |  " << tab[8] << "  |  " << tab[9] << endl;
-
-    cout << "     |     |     " << endl << endl;
-    if (i == 1)
-
-        cout << "==>\aJugador " << --jug << " gana ";
-    else
-        cout << "==>\aEmpate!";
-
-    cin.ignore();
-    cin.get();
+        mostrarTablero(Board);  //Aqui se reemplaza para la funcion repetida
+        Player = (Player % 2) ? 1 : 2;
+        identifier = (Player == 1) ? 'X' : 'O';
+        cout << "Jugador " << Player << ", ingresa un numero:  ";
+        cin >> Position;
+        makeMovement(Position, identifier, &Player);
+        existsWinner = checkWinner();
+        Player++;
+        jugadas++;
+    } while (!existsWinner && jugadas!=9);
+    mostrarTablero(Board);   //Aqui se reemplaza para la funcion repetida
+    showresult(existsWinner, Player);    //Aqui se verifica el ganador
     return 0;
 }
 
